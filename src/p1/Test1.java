d366bbaefffa8feecab55c336e0edb721735f3f9
sveta.kanani <sveta.kanani123@gmail.com>
2024-10-15 11:43:07 +0530

    project upload

---
+++ src/p1/Test1.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("In Test1 file");
+	}
+	
+}
---
+++ src/p2/Test2.java
@@ -0,0 +1,9 @@
+package p2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("In Test2 file");
+	}
+	
+}
